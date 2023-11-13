621ff7e3a4e342df7b09b23c47f8d9a7fe0ed1fb
abir malek <abir.malek87@gmail.com>
2023-11-13 16:58:45 +0100

    updated

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,7 +10,13 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @questions = params [:question]
-    @answer = coach_answer(@question)
+    @question = params[:question]
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question[-1] == "?"
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
