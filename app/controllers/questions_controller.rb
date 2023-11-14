0bfaa056916e73c54eb8f9936fc36d6385b7ba85
abir malek <abir.malek87@gmail.com>
2023-11-14 19:27:15 +0100

    challange completed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,8 +11,10 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
+    #abort(question)
     if @question == 'I am going to work'
       @answer = 'Great!'
+    # elsif @question.end_with?("?")
     elsif @question[-1] == "?"
       @answer = 'Silly question, get dressed and go to work!'
     else
